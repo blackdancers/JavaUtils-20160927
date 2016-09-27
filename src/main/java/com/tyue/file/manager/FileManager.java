ff191942b912b4989faa691f96071a974a449ba4
liudebin <liudebin@99114.com>
2016-09-27 13:56:39 +0800

    1

---
+++ src/main/java/com/tyue/file/manager/FileManager.java
@@ -0,0 +1,19 @@
+package com.tyue.file.manager;
+
+/**
+ * 文件操作类<p>
+ * 1、删除文件夹下指定名称的文件
+ * 
+ * @author Admin
+ *
+ */
+public class FileManager {
+	
+	
+	
+	
+	public static void main(String[] args) {
+
+	}
+
+}
